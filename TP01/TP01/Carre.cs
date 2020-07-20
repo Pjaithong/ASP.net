b89ac2590d7ac1009793122eb27bd0037e7decb2
Patcha Danet <patcha.danet@gmail.com>
2020-07-20 15:54:23 +0200

    tp01

---
+++ TP01/TP01/Carre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TP01
+{
+    public class Carre : Forme
+    {
+        public int Longueur { get; set; }
+
+        public override double Aire => Longueur*Longueur;
+
+        public override double Perimetre => 4*Longueur;
+
+        public override string ToString()
+        {
+            return $"Carré de coté {Longueur}" + Environment.NewLine + base.ToString();
+        }
+    }
+}
---
+++ TP01/TP01/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TP01
+{
+    public class Rectangle : Forme
+    {
+        public int Largeur { get; set; }
+        public int Longueur { get; set; }
+
+        public override double Aire => Largeur*Longueur;
+
+        public override double Perimetre => 2*Largeur + 2*Longueur;
+
+        public override string ToString()
+        {
+            return $"Ractangle de longueur = {Longueur} et largeur = {Largeur}" + Environment.NewLine + base.ToString();
+        }
+    }
+}
