ed7ce0e59fd18adf3ed404a4126a8a3536db1ef8
Patcha Danet <patcha.danet@gmail.com>
2020-07-23 13:59:45 +0200

    tp pizza 01-4

---
+++ TP01/TpPizzas01/Helpers/PizzaHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TpPizzas01.Helpers
+{
+    public static class PizzaHelpers
+    {
+        /// <summary>
+        /// Creates simple button
+        /// <div class="form-group">
+        ///     <div class="col-md-offset-2 col-md-10">
+        ///         <input type = "submit" value="@name" class="btn btn-default" />
+        ///     </div>
+        /// </div>
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static IHtmlString CostumSubmit(this HtmlHelper helper, string name)
+        {  
+            StringBuilder res = new StringBuilder();
+            res.Append("<div class=\"form - group\">");
+            res.Append("<div class=\"col - md - offset - 2 col - md - 10\">");
+            res.Append($"<input type = \"submit\" value=\"{@name}\" class=\"btn btn - default\" />");
+            res.Append("</div>");
+            res.Append("</div>");
+
+            return new MvcHtmlString(res.ToString());
+        }
+    }
+}
